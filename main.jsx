b38efbe63a243c8813debd4194eb01f16e339212
shwetha-2003 <108446664+shwetha-2003@users.noreply.github.com>
2024-05-08 18:59:49 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import App from './App'
+import './index.css'
+
+import { theme } from './Theme';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
+)
